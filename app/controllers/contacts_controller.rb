7157faafe2c3219f03ecabe32bba5772acbd8f6b
Björn Gröger <bjoern_groeger@web.de>
2016-12-30 22:22:32 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
+  @contact = Contact.new
   end
+  
+  def create
+  @contact = Contact.new(contact_params)
+  if @contact.save
+     redirect_to new_contact_path, notice: "Nachricht gesendet."
+  else
+     redirect_to new_contact_path, notice: "Nachricht nicht gesendet."
+  end
+  end
+private
+  def contact_params
+     params.require(:contact).permit(:Name, :email, :Nachricht)
+  end
+  
 end
